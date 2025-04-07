c0e4a0358670e3dcac79a307db2e3a2ac3c8666e
32192372Alejo <alejo290816@gmail.com>
2025-04-07 10:42:18 -0500

    Add app/src/main/java/com/example/interviewapp/MainActivity.kt 1744040537984

--- app/src/main/java/com/example/interviewapp/MainActivity.kt
+++ app/src/main/java/com/example/interviewapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.interviewapp
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
